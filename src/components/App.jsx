1d167ccd31aed332c39bb59c51a7fb4a9f5dcae5
Eeli K <eelikivi@hotmail.com>
2020-08-10 22:57:37 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+	const text = 'test';
+
+	return (
+		<div>{text}</div>
+	);
+}
---
+++ webpack.config.babel.js
@@ -0,0 +1,39 @@
+import path from 'path';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
+
+export default () => ({
+	entry: './src/index.jsx',
+	resolve: {
+		extensions: ['.js', '.jsx'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			},
+			{
+				test: /\.html$/,
+				use: {
+					loader: 'html-loader',
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+	],
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js',
+	},
+});
